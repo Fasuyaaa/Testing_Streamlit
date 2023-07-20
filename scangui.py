d20a2c1b30c69d5652fbcc160567b426732b69c3
Dhavin Fasya Alviyanto <113501500+Fasuyaaa@users.noreply.github.com>
2023-07-21 06:54:54 +0700

    Update scangui.py

--- scangui.py
+++ scangui.py
@@ -1,38 +1,33 @@
 import streamlit as st
-import smtplib
-from email.message import EmailMessage
 
-def send_email(sender_name, message_content):
-    # Replace the following variables with your email account details
-    email_address = "your_email@gmail.com"
-    email_password = "your_email_password"
+# Panel admin password
+ADMIN_PASSWORD = "123"
 
-    msg = EmailMessage()
-    msg["Subject"] = "Pesan dari Aplikasi Streamlit"
-    msg["From"] = email_address
-    msg["To"] = "dhavinf.a@gmail.com"
-    msg.set_content(f"{sender_name} mengirim pesan: \n\n{message_content}")
-
-    try:
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-            server.login(email_address, email_password)
-            server.send_message(msg)
-        st.success("Pesan berhasil dikirim!")
-    except Exception as e:
-        st.error(f"Terjadi kesalahan saat mengirim email: {str(e)}")
+# Function to send a message to the admin panel
+def send_message(sender_name, message_content):
+    with open("messages.txt", "a") as file:
+        file.write(f"{sender_name}: {message_content}\n")
 
 def main():
-    st.title("Aplikasi Kirim Pesan")
-    st.write("Isi form di bawah ini untuk mengirim pesan.")
+    st.title("Aplikasi Kirim Pesan dengan Panel Admin")
+    
+    # Check if the user is the admin
+    if st.text_input("Masukkan kata sandi:", type="password") == ADMIN_PASSWORD:
+        st.subheader("Panel Admin")
+        with open("messages.txt", "r") as file:
+            messages = file.read()
+        st.text_area("Pesan yang Dikirim", messages, height=200)
+    else:
+        st.subheader("Kirim Pesan")
+        sender_name = st.text_input("Nama Pengirim:")
+        message_content = st.text_area("Isi Pesan:")
 
-    sender_name = st.text_input("Nama Pengirim:")
-    message_content = st.text_area("Isi Pesan:")
-
-    if st.button("Kirim Pesan"):
-        if sender_name and message_content:
-            send_email(sender_name, message_content)
-        else:
-            st.warning("Mohon isi nama pengirim dan isi pesan terlebih dahulu.")
+        if st.button("Kirim Pesan"):
+            if sender_name and message_content:
+                send_message(sender_name, message_content)
+                st.success("Pesan berhasil dikirim!")
+            else:
+                st.warning("Mohon isi nama pengirim dan isi pesan terlebih dahulu.")
 
 if __name__ == "__main__":
     main()
